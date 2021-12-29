daf61539c7bda185ca6e712afdf32a82b0b08592
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2021-12-29 17:14:10 -0500

    refactor: refactors layout page

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,25 +1,19 @@
 import { FC } from 'react';
-import AdminLayout from './AdminLayout';
 import DefaultLayout from './DefaultLayout';
-import LoginLayout from './LoginLayout';
-import ProfileLayout from './ProfileLayout';
 
 export const AllLayouts = {
-  login: LoginLayout,
-  admin: AdminLayout,
-  default: DefaultLayout,
-  profile: ProfileLayout
+    default: DefaultLayout,
 };
 
 export type LayoutType = {
-  Layout?: keyof typeof AllLayouts;
-  Role?: string | string[];
+    Layout?: keyof typeof AllLayouts;
+    Role?: string | string[];
 };
 
 const LayoutContext: FC<LayoutType> = (props) => {
-  const { Layout, children, Role } = props;
-  const GetLayout = AllLayouts[Layout || 'default'];
-  return <GetLayout Role={Role}>{children}</GetLayout>;
+    const { Layout, children, Role } = props;
+    const GetLayout = AllLayouts[Layout || 'default'];
+    return <GetLayout Role={Role}>{children}</GetLayout>;
 };
 
 export default LayoutContext;
