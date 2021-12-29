638ed0aa958b363bd906145e6cb95c22b71ac59b
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2021-12-29 18:03:03 -0500

    feat: sets fonts for the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => (
-  <Html>
-    <Head />
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
+    <Html>
+        <Head>
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link
+                rel='preconnect'
+                href='https://fonts.gstatic.com'
+                crossOrigin='true'
+            />
+            <link
+                href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&family=Poppins:wght@200;400;600&display=swap'
+                rel='stylesheet'
+            />
+        </Head>
+        <body>
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
 );
 
 export default Document;
